6d6cc6ebb4078c8b203636852ddec127b8125341
Ahn <cwahn0904@gmail.com>
2021-03-07 19:43:38 +0900

    Add firmware uploader
    Firmware uploader works on local env.

--- ota/urls.py
+++ ota/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('firmware', views.firmware_upload, name='firmware'),
+    path('get_latest_firmware', views.get_latest_firmware, name='get_latest_firmware'),
 ]
--- ota/views.py
+++ ota/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import *
+
+from django_file_md5 import calculate_md5
 
 # Create your views here.
 def index(request):
@@ -17,5 +20,16 @@ def firmware_upload(request):
 
     elif (request.method == 'GET'):
         return render(request, 'ota/firmware.html', {})
+
+def get_latest_firmware(request):
+    latest_firmware = ContFirmware.objects.latest('datetime')
+    md5 = calculate_md5(latest_firmware.firmware)
+    base_url =  "{0}://{1}".format(request.scheme, request.get_host())
+    url = base_url + latest_firmware.firmware.url
+    return JsonResponse({
+        'name': latest_firmware.name,
+        'md5': md5,
+        'url': url,
+    })
 
         
